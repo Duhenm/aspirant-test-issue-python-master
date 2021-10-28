83f851010bad27a8527e882e1a2a7d10e952c1ea
Duhenm <77613285+Duhenm@users.noreply.github.com>
2021-10-28 18:05:35 +0500

    Добавлены модели PicnicModal PicnicRegistrationModal

--- src/models.py
+++ src/models.py
@@ -1,12 +1,27 @@
 from pydantic import BaseModel, Field
+from datetime import datetime
 
 """
 Добавлено ограничение по возрасту 
 """
+
+
 class RegisterUserRequest(BaseModel):
     name: str = Field(min_length=2)
     surname: str = Field(min_length=2)
     age: int = Field(gt=0, le=1000)
+
+
+class CityModal(BaseModel):
+    name: str = Field(min_length=2)
+
+
+class PicnicRegistrationModal(BaseModel):
+    user_id: int
+    picnic_id: int
+
+    class Config:
+        orm_mode = True
 
 
 class UserModel(BaseModel):
@@ -14,6 +29,14 @@ class UserModel(BaseModel):
     name: str
     surname: str
     age: int
+
+    class Config:
+        orm_mode = True
+
+
+class PicnicModal(BaseModel):
+    city_id: int
+    time: datetime
 
     class Config:
         orm_mode = True
